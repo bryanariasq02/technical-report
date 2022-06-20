e446ad12f7d354b246662f25f8950edd835cd05e
Camilo García <72902253+camilogs1@users.noreply.github.com>
2022-06-20 17:43:39 -0500

    update

--- Streamlit/database.py
+++ Streamlit/database.py
@@ -48,10 +48,22 @@ with row1_2:
 st.subheader('Opción 1:')
 st.image(image, caption='Tamaño de la base de datos')
 
-st.subheader('Tiempo ejecución')
+st.subheader('Tiempos de ejecución')
 row1_3, row1_4 = st.columns((2, 3))
 
 with row1_3:
-    st.subheader('Tiempo categorias GroupLAC:')
-    st.metric(label="Colecciones", value="50")
-    st.markdown(categorias_individual)
+    st.subheader('Tiempo categorias GroupLAC en minutos:')
+    st.metric(label="Categorías:", value="50")
+    st.metric(label="Promedio tiempo lectura, limpieza y subida por categoría:", value=promedio_categoria)
+    st.metric(label="Suma tiempo subida de archivos por categoría:", value=suma_subida_categorias)
+    st.metric(label="Tiempo total 50 categorías GrupLAC:", value=tiempo_general_GrupLAC)
+    
+with row1_4:
+    st.subheader('Tiempo categorias SiB en segundos:')
+    st.metric(label="Categorías:", value="1")
+    st.metric(label="Tiempo ejecución función SiB:", value = tiempo_SiB_general)
+    st.metric(label="Tiempo subida de archivos SiB:", value = tiempo_SiB_subida)
+    st.metric(label="Total tiempo categoría SiB:", value = total_tiempo_SiB) 
+    
+st.subheader('Tiempo total 51 categorías en horas:')
+st.metric(label="", value = tiempo_total)
